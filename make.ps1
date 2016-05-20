768f3491d5561abdfb8a4a2d5f8833c98214792d
CH.Idea <sbw228@gmail.com>
2016-05-21 01:24:23 +0900

    compile script added

--- make.ps1
+++ make.ps1
@@ -1,7 +1,43 @@
-$arr = (ls bower_components).Name
+$arr = (ls bower_components demo.html -Recurse)
+$b = ''
+$h = ''
 foreach($a in $arr){
-  (ls "bower_components\$a").Name
-  if ((ls "bower_components\$a").Name -contains 'demo'){
-    echo $a
+  $body=$false
+  $p=$a.PSParentPath+"\"+$a.Name
+  echo $p
+  $a=$a.PSParentPath -replace '.+\\'
+  foreach($l in (cat $p)){
+    if(($l -like '*prettify*')){
+      $l
+      exit
+    }
+    if(($l.trimstart().startswith('<script ') -or `
+       ($l.trimstart().startswith('<link ')))  -and `
+      !($l -like '*/webcomponents.js*' -or `
+        $l -like '*/platform.js*' -or `
+        $l -like '*/polymer.html*')){
+      $h += $l -replace 'href="', "href=`"bower_components/$a/" `
+        -replace 'src="', "src=`"bower_components/$a/"
+    }
+    if(!$body){
+      $body=$l.startswith('<body ')
+    }else{
+      $body=!$l.startswith('</body>')
+      if($body){
+        $b += $l -replace 'href="', "href=`"bower_components/$a/" `
+          -replace 'src="', "src=`"bower_components/$a/"
+      }
+    }
   }
 }
+
+"<html>
+<head>
+  <script src=`"bower_components/webcomponentsjs/webcomponents.js`"></script>
+$h
+</head>
+<body>
+  <script src=`"bower_components/polymer/polymer.html`"></script>
+$b
+</body>
+</html>" | sc test.html -Encoding utf8
